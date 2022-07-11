53a8ba8dd3256b95cfb9ddba0ea5787e0551232f
Carlos Eduardo <c.eduardo.jesus@outlook.com>
2022-07-11 11:18:59 -0500

    codigo de otros 3

---
+++ filtro.js
@@ -0,0 +1,63 @@
+// Tenemos un li de productos
+
+const productos = [
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+]
+
+const li = document.getElementsByName("lista-de-productos")
+const $i = document.querySelector('.input');
+
+for (let i = 0; i < productos.length; i++) {
+  var d = document.createElement("div")
+  d.classList.add("producto")
+
+  var ti = document.createElement("p")
+  ti.classList.add("titulo")
+  ti.textContent = productos[i].nombre
+  
+  var imagen = document.createElement("img");
+  imagen.setAttribute('src', productos[i].img);
+
+  d.appendChild(ti)
+  d.appendChild(imagen)
+
+  li.appendChild(d)
+}
+
+displayProductos(productos)
+const botonDeFiltro = document.querySelector("button");
+
+botonDeFiltro.onclick = function() {
+  while (li.firstChild) {
+    li.removeChild(li.firstChild);
+  }
+
+  const texto = $i.value;
+  console.log(texto);
+  const productosFiltrados = filtrado(productos, texto );
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+  
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productosFiltrados[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productosFiltrados[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
+}
+
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+}  
